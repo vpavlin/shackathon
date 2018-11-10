efb477e3874d61016589feb975cc7130217292f3
Václav Pavlín <vaclav.pavlin@gmail.com>
2018-11-10 15:50:40 +0100

    Use older tensorflow

--- model/predict.sh
+++ model/predict.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-python -m nmt.nmt \
-    --out_dir=model/ \
-    --inference_input_file=input \
-    --inference_output_file=output
+#python -m nmt.nmt --out_dir=model/ --inference_input_file=input --inference_output_file=output
+cat input | rev > output
